d4532c8c8d969c9516f8ba996c15b09f94ee9b44
JaiS739 <jksingh00739@gmail.com>
2022-10-06 17:22:24 +0530

    error handling file

---
+++ errorHandling/index.js
@@ -0,0 +1,16 @@
+// console.log(a); //undefined
+
+// console.log(b); // reference error: cannot access b before its initialisation.
+
+// console.log(c); // reference error: cannot access b before its initialisation.
+
+// var a = 5;
+// let b = 6;
+// const c = 7;
+
+// -------------------xxxxxxxxxxxxxx---------------
+
+// const  a;
+// a = 4;
+
+// console.log(a);   //SyntaxError: Missing initializer in const declaration
